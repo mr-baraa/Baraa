ba8544507432bce594bf7a31e4f66e2086244eda
Baraa <89737291+mr-baraa@users.noreply.github.com>
2022-07-10 21:16:05 +0300

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -1,20 +1,23 @@
-function sec2() {
-  document.getElementById("sec1").style.display = "none";
-  document.getElementById("sec4").style.display = "none";
-  document.getElementById("footer").style.display = "flex";
-
-  var x = (document.getElementById("sec2").style.display = "flex");
-  document.getElementById("audio").play();
-  document.getElementById("sec3").style.display = "none";
-}
-
 function sec1() {
+  document.getElementById("sec5").style.display = "none";
+
   document.getElementById("sec2").style.display = "none";
   document.getElementById("sec3").style.display = "none";
   document.getElementById("sec4").style.display = "none";
   document.getElementById("footer").style.display = "flex";
-
   var x = (document.getElementById("sec1").style.display = "flex");
+
+  document.getElementById("audio").play();
+}
+
+function sec2() {
+  document.getElementById("sec1").style.display = "none";
+  document.getElementById("sec4").style.display = "none";
+  document.getElementById("footer").style.display = "flex";
+  var x = (document.getElementById("sec2").style.display = "flex");
+  document.getElementById("sec3").style.display = "none";
+  document.getElementById("sec5").style.display = "none";
+
   document.getElementById("audio").play();
 }
 
@@ -24,10 +27,10 @@ function sec3() {
   document.getElementById("sec4").style.display = "none";
   document.getElementById("sec3").style.display = "flex";
   document.getElementById("footer").style.display = "none";
-
   document.getElementById("alert-additional-content-3").classList = "alert";
   var x = (document.getElementById("sec3").style.display = "flex");
   document.getElementById("audio").play();
+  document.getElementById("sec5").style.display = "none";
 }
 
 function sec4() {
@@ -35,8 +38,20 @@ function sec4() {
   document.getElementById("sec3").style.display = "none";
   document.getElementById("sec1").style.display = "none";
   document.getElementById("footer").style.display = "flex";
-
   var x = (document.getElementById("sec4").style.display = "flex");
+  document.getElementById("sec5").style.display = "none";
+
+  document.getElementById("audio").play();
+}
+
+function sec5() {
+  document.getElementById("sec2").style.display = "none";
+  document.getElementById("sec3").style.display = "none";
+  document.getElementById("sec1").style.display = "none";
+  document.getElementById("sec4").style.display = "none";
+
+  document.getElementById("footer").style.display = "flex";
+  var x = (document.getElementById("sec5").style.display = "flex");
   document.getElementById("audio").play();
 }
 
